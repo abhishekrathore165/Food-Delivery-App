93934441679126b9806c010c8a3a433cce1558fa
abhishekrathore165 <156427832+abhishekrathore165@users.noreply.github.com>
2024-06-26 18:19:17 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose  from "mongoose";
 
 export  const connectDB = async ()=>{
-    await mongoose.connect('mongodb://localhost:27017/food-del')
+    await mongoose.connect('mongodb://[::1]:27017/food-del')
     .then(()=>console.log('DB Connected'))
 }
