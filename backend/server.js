8884b95e3c81326bb282d9b259d407c24ae0f78f
abhishekrathore165 <156427832+abhishekrathore165@users.noreply.github.com>
2024-06-26 11:15:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import 'dotenv/config'
 import cartRouter from "./routes/cartroute.js";
 import orderRouter from "./routes/orderRoute.js";
 
-
+const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 4000;
 
